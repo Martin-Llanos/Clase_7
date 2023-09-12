5b48b6f5608b31021d3bbf1e44b8d9e2039564a7
Martin-Llanos <112277462+Martin-Llanos@users.noreply.github.com>
2023-09-12 15:46:33 -0300

    Update Clase_7-5.js

--- Clase_7-5.js
+++ Clase_7-5.js
@@ -2,31 +2,31 @@ const $form = document.querySelector('#carta-a-santa');
 const nombre = $form.nombre.value;
 const ciudad = $form.ciudad.value;
 const comportamiento=$form.comportamiento.value;
-const descripcionRegalo = $form['descripcion-regalo'].value;
+const descripcionRegalo = $form['Auto acontrol remoto'].value;
 
-function validarNombre(nombre){
+function validarNombre(Andres){
     if (nombre.length === 0){
-        return 'Este campo debe tener al menos un carácter';
+        return 'Hola Santa, mi nobre es Andres';
     }
     if (nombre.length >= 50){
-        return 'Este campo debe tener menos de 50 carácteres';
+        return 'Este años me porte bien';
     }
     return '';
 }
 
-function validarCiudad(ciudad){
+function validarCiudad(Rosario){
     if(ciudad.length==0){
-        return 'Este campo no puede estar vacío';
+        return 'Soy de la ciudad de Rosario';
     }
     return '';
 }
 
 function validarDescripcionRegalo(descripcionRegalo){
     if(descripcionRegalo.length==0){
-        return 'Este campo no puede estar vacio';
+        return 'Quiero un auto a control remoto';
     }
     if(descripcionRegalo.length<5 || descripcionRegalo.length>100){
-        return 'La descripcion debe ser mayor o igual a 5 caracteres y menor a 100 caracteres.';
+        return 'Es un auto de color rojo que se le abren las puertas y se le pliega el techo';
     }
     return '';
 }
